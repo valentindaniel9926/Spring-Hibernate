510934591f93c33725bde74618529f79c83ed5b4
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-09-23 23:10:16 +0300

    Add files via upload

---
+++ Spring MVC Form Validation - Creating Custom Validation Rules/Custom Validation Rule/src/com/luv2code/springdemo/mvc/Customer.java
@@ -0,0 +1,84 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+
+public class Customer {
+
+	private String firstName;
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private String lastName;
+	
+	
+	@NotNull(message="is required")
+	@Min(value=0, message="must be greater than or equal to zero")
+	@Max(value=10, message="muss be less than or equal to ten")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}" , message="only 5 chars/digits")
+	private String postalCode;
+	
+	@CourseCode(value="UDEMY", message="must start with UDEMY")
+	private String courseCode;
+	
+	
+	
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+
+	public void setFreePasses(Integer  freePasses) {
+		this.freePasses = freePasses;
+	}
+
+}
