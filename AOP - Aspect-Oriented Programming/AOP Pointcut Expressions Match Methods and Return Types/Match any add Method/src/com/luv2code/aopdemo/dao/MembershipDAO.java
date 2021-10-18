fd3139b2adf1dfb1b91e344588aef245ee3ff858
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-10-18 16:12:37 +0300

    Add files via upload

---
+++ AOP - Aspect-Oriented Programming/AOP Pointcut Expressions Match Methods and Return Types/Match any add Method/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,14 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+	
+	public void addGreatMember() {
+		
+		System.out.println(getClass() + ": Doing Stuff :  ADDING A MEMBERSHIP ACCOUNT");
+		
+	}
+
+}
