cbb7070860e190a13886637c95356e40e7f603ec
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-10-30 09:39:58 +0300

    Add files via upload

---
+++ Maven/mygreatapp Simple Project/src/main/java/com/luv2code/mygreatapp/App.java
@@ -0,0 +1,13 @@
+package com.luv2code.mygreatapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World of MAVEN!" );
+    }
+}
