6ac12698869a90a2854e14a9b8e94d09223f938b
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-11-04 09:59:04 +0200

    Add files via upload

---
+++ Spring Security/Manually Adding Cross Site Request Forgery Token(CSFR)/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("valentin").password("topsecret123").roles("EMPLOYEE");
+		auth.inMemoryAuthentication().withUser("daniel").password("topsecret123").roles("MANAGER");
+		auth.inMemoryAuthentication().withUser("andrew").password("topsecret123").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.antMatchers("/css/**").permitAll()
+				.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+				.and()
+				.logout().permitAll();
+	}
+		
+}
