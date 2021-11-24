1e128ad49d49bc29dd624e910f0eda08595de317
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-11-24 11:40:42 +0200

    Add files via upload

---
+++ REST CRUD API with Hibernate/Rest Controller Methods/ServiceLayer/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+	private EmployeeService employeeService;
+	
+	@Autowired
+	public EmployeeRestController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+	
+	// expose "/employees" and return list of employees
+	@GetMapping("/employees")
+	public List<Employee> findAll() {
+		return employeeService.findAll();
+	}
+
+	
+}
